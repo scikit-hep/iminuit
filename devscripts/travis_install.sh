249c9ce14891198230caf197ed2ba70670912636
Christoph Deil <Deil.Christoph@gmail.com>
2012-12-20 10:32:37 +0100

    travis-ci: fix bash script error

--- devscripts/travis_install.sh
+++ devscripts/travis_install.sh
@@ -1,8 +1,8 @@
 if [ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]; then
   # For Python 2.7 we can install binary packages with apt-get
   # We use this to check the notebooks
-  deactivate;
-  time sudo apt-get install -qq python-nose python-matplotlib ipython-notebook cython;
+  deactivate
+  time sudo apt-get install -qq python-nose python-matplotlib ipython-notebook cython
 fi
 #- time pip install ipython[zmq,notebook] --use-mirrors
 #- time pip install cython --use-mirrors
