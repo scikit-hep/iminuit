a350e663a859015bc671e78511339807c9f4adf3
Christoph Deil <Deil.Christoph@gmail.com>
2014-11-07 22:54:45 +0100

    Rename test files to snake_case (PEP8)

--- iminuit/test/test_functions.py
+++ iminuit/test/test_functions.py
@@ -2,8 +2,7 @@ import random
 from math import sqrt, exp, cos, pi, e
 from nose.tools import assert_almost_equal
 from iminuit import Minuit
-
-from .testiminuit import assert_array_almost_equal
+from .test_iminuit import assert_array_almost_equal
 
 random.seed(0.258)
 
