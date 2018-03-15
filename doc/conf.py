13d3685a3dc8a2604aaaf866b0f65ec51c4eef8f
Hans Dembinski <hans.dembinski@gmail.com>
2018-03-15 18:08:28 +0100

    fix for readthedocs, dont try to import local build on readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -25,11 +25,6 @@
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 
-# Make sure local build of iminuit is found, if it is available
-import os
-import sys
-sys.path.insert(0, os.path.dirname(__file__) + "/..")
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
@@ -267,7 +262,12 @@ texinfo_documents = [
 import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
+if not on_rtd:
+    # Make sure local build of iminuit is found
+    import sys
+    sys.path.insert(0, os.path.dirname(__file__) + "/..")
+
+    # Import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
