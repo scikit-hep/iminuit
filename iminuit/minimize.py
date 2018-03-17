c82fe9ca11ab991e2735e1a4d220e721e9cdbd06
Hans Dembinski <hans.dembinski@gmail.com>
2018-03-17 15:58:10 +0100

    support and test all arguments of scipys minimize

--- iminuit/minimize.py
+++ iminuit/minimize.py
@@ -1,8 +1,13 @@
 from iminuit import Minuit
 from scipy.optimize import OptimizeResult
 from numpy import array
+import warnings
 
-def minimize(fun, x0, method=None, jac=None, bounds=None, callback=None, options=None):
+def minimize(fun, x0, args=(), method=None,
+             jac=None, hess=None, hessp=None,
+             bounds=None, constraints=(),
+             tol=None,
+             callback=None, options=None):
     """Imitates the interface of the SciPy method of the same name in scipy.optimize.
 
     For argument description, see scipy.optimize.minimize.
@@ -13,13 +18,6 @@ def minimize(fun, x0, method=None, jac=None, bounds=None, callback=None, options
         m = method.lower()
         if m != "migrad":
             raise ValueError("Unknown solver " + m)
-    names = ['%i'%i for i in range(len(x0))]
-    kwargs = {}
-    for i, x in enumerate(names):
-        kwargs[x] = x0[i]
-        kwargs['error_'+x] = 0.1 if x0[i] == 0 else abs(0.1 * x0[i])
-        if bounds:
-            kwargs['limit_'+x] = bounds[i]
 
     nfev = 0
     njev = 0
@@ -27,25 +25,36 @@ def minimize(fun, x0, method=None, jac=None, bounds=None, callback=None, options
     class Wrapped:
         fun = None
         jac = None
+        args = ()
         callback = None
         nfev = 0
         njev = 0
-        def __init__(self, fun, jac, callback):
+        def __init__(self, fun, jac, args, callback):
             self.fun = fun
             self.jac = jac
+            self.args = args
             self.callback = callback
         def func(self, *args):
             self.nfev += 1
             x = array(args)
             if self.callback:
                 self.callback(x)
-            return self.fun(x)
+            return self.fun(x, *self.args)
         def grad(self, *args):
             self.njev += 1
             x = array(args)
-            return self.jac(x)
+            return self.jac(x, *self.args)
 
-    wrapped = Wrapped(fun, jac, callback)
+    wrapped = Wrapped(fun, jac, args, callback)
+
+    if hess or hessp:
+        warnings.warn("hess and hessp arguments cannot be handled and are ignored")
+
+    if constraints:
+        raise ValueError("Minuit only supports bounds, not constraints")
+
+    if tol:
+        warnings.warn("tol argument has no effect on Minuit")
 
     print_level = 0
     maxiter = 10000
@@ -54,6 +63,14 @@ def minimize(fun, x0, method=None, jac=None, bounds=None, callback=None, options
             print_level = 1
         if "maxiter" in options:
             maxiter = options["maxiter"]
+
+    names = ['%i'%i for i in range(len(x0))]
+    kwargs = {}
+    for i, x in enumerate(names):
+        kwargs[x] = x0[i]
+        kwargs['error_'+x] = 0.1 if x0[i] == 0 else abs(0.1 * x0[i])
+        if bounds:
+            kwargs['limit_'+x] = bounds[i]
 
     m = Minuit(wrapped.func,
                grad_fcn=wrapped.grad if jac else None,
