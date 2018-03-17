673c40a2675f54831acf36fe9dea6a913c69752a
Hans Dembinski <hans.dembinski@gmail.com>
2018-03-17 15:06:58 +0100

    added minimize interface for Minuit

--- iminuit/minimize.py
+++ iminuit/minimize.py
@@ -1,55 +1,25 @@
 from iminuit import Minuit
-import warnings
+from scipy.optimize import OptimizeResult
+from numpy import array
 
-
-class OptimizeResult(dict):
-    """Imitation of scipy.optimize.OptimizeResult."""
-    def __init__(self, **kwargs):
-        dict.__init__(self, **kwargs)
-    def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(name)
-
-
-def minimize(fun, x0, args=(), method=None,
-             jac=None, hess=None, hessp=None,
-             bounds=None, constraints=(),
-             tol=None, callback=None, options=None):
+def minimize(fun, x0, method=None, jac=None, bounds=None, callback=None, options=None):
     """Imitates the interface of the SciPy method of the same name in scipy.optimize.
 
-    For a general description of the arguments, see scipy.optimize.minimize.
+    For argument description, see scipy.optimize.minimize.
 
     The only supported method is 'Migrad'.
-
-    The `options` argument can be used to pass special settings to Minuit.
-    All are optional.
-
-    **Options**
-
-    - *disp* (bool): Set to true to print convergence messages. Default: False.
-    - *maxfev* (int): Maximum allowed number of iterations. Default: 10000.
-    - *eps* (sequence): Initial step size to numerical compute derivative.
-      Minuit automatically refines this in subsequent iterations and is very
-      insensitive to the initial choice. Default: 1.
-
-    **Returns: OptimizeResult** (dict with attribute access)
-    - *x* (ndarray): Solution of optimization.
-    - *fun* (float): Value of objective function at minimum.
-    - *message* (str): Description of cause of termination.
-    - *hess_inv* (ndarray): Inverse of Hesse matrix at minimum (may not be exact).
-    - nfev (int): Number of function evaluations.
-    - njev (int): Number of jacobian evaluations.
-    - minuit (object): Minuit object internally used to do the minimization.
-      Use this to extract more information about the parameter errors.
     """
-    import numpy as np
-
     if method:
         m = method.lower()
         if m != "migrad":
             raise ValueError("Unknown solver " + m)
+    names = ['%i'%i for i in range(len(x0))]
+    kwargs = {}
+    for i, x in enumerate(names):
+        kwargs[x] = x0[i]
+        kwargs['error_'+x] = 0.1 if x0[i] == 0 else abs(0.1 * x0[i])
+        if bounds:
+            kwargs['limit_'+x] = bounds[i]
 
     nfev = 0
     njev = 0
@@ -57,84 +27,44 @@ def minimize(fun, x0, args=(), method=None,
     class Wrapped:
         fun = None
         jac = None
-        args = ()
         callback = None
         nfev = 0
         njev = 0
-        np_array = np.array
-        def __init__(self, fun, jac, args, callback):
+        def __init__(self, fun, jac, callback):
             self.fun = fun
             self.jac = jac
-            self.args = args
             self.callback = callback
         def func(self, *args):
             self.nfev += 1
-            x = self.np_array(args)
+            x = array(args)
             if self.callback:
                 self.callback(x)
-            return self.fun(x, *self.args)
+            return self.fun(x)
         def grad(self, *args):
             self.njev += 1
-            x = self.np_array(args)
-            return self.jac(x, *self.args)
+            x = array(args)
+            return self.jac(x)
 
-    wrapped = Wrapped(fun, jac, args, callback)
+    wrapped = Wrapped(fun, jac, callback)
 
-    if hess or hessp:
-        warnings.warn("hess and hessp arguments cannot be handled and are ignored")
-
-    if constraints:
-        raise ValueError("Minuit only supports bounds, not constraints")
-
-    if tol:
-        warnings.warn("tol argument has no effect on Minuit")
-
-    kwargs = dict(errordef=1, print_level=0)
-    if jac:
-        kwargs['grad_fcn'] = wrapped.grad
-
-    maxfev = 10000
-    eps = None
+    print_level = 0
+    maxiter = 10000
     if options:
         if "disp" in options:
-            kwargs['print_level'] = 1
+            print_level = 1
         if "maxiter" in options:
-            warnings.warn("maxiter not supported, acts like maxfev instead")
-            maxfev = options["maxiter"]
-        if "maxfev" in options:
-            maxfev = options["maxfev"]
-        if "eps" in options:
-            eps = options["eps"]
-    if eps is None:
-        eps = np.ones(len(x0))
+            maxiter = options["maxiter"]
 
-    names = ['%i'%i for i in range(len(x0))]
-    for i, x in enumerate(names):
-        kwargs[x] = x0[i]
-        kwargs['error_'+x] = eps[i]
-        if bounds:
-            kwargs['limit_'+x] = bounds[i]
-    kwargs['forced_parameters'] = names
-
-    m = Minuit(wrapped.func, **kwargs)
-    m.migrad(ncall=maxfev)
-
-    message = "Optimization terminated successfully."
-    success = m.migrad_ok()
-    if not success:
-        message = "Optimization failed."
-        fmin = m.get_fmin()
-        if fmin.has_reached_call_limit:
-            message += " Call limit was reached."
-        if fmin.is_above_max_edm:
-            message += " Estimated distance to minimum too large."
+    m = Minuit(wrapped.func,
+               grad_fcn=wrapped.grad if jac else None,
+               forced_parameters=names,
+               print_level=0, errordef=1,
+               **kwargs)
+    m.migrad(ncall=maxiter)
 
     return OptimizeResult(x=m.np_values(),
-                          success=success,
+                          success=m.migrad_ok(),
                           fun=m.fval,
                           hess_inv=m.np_covariance(),
-                          message=message,
                           nfev=wrapped.nfev,
-                          njev=wrapped.njev,
-                          minuit=m,
-                          )
+                          njev=wrapped.njev)
--- iminuit/tests/test_minimize.py
+++ iminuit/tests/test_minimize.py
@@ -3,49 +3,24 @@ from iminuit import minimize
 from iminuit.tests.utils import assert_allclose
 import numpy as np
 
+def func(x):
+    return 1 + x[0] ** 2 + (x[1] - 1) ** 2 + (x[2] - 2) ** 2
 
-def func(x, *args):
-    c = args[0] if args else 1
-    return c + x[0] ** 2 + (x[1] - 1) ** 2 + (x[2] - 2) ** 2
-
-def grad(x, *args):
+def grad(x):
     return np.array((2 * x[0], 2 * (x[1] - 1), 2 * (x[2] - 2)))
 
 
 def test_simple():
-    result = minimize(func, (1, 1, 1))
+    result = minimize(func, np.ones(3))
     assert_allclose(result.x, (0, 1, 2), atol=1e-8)
     assert_allclose(result.fun, 1)
-    assert result.nfev > 0
-    assert result.njev == 0
-
-
-def test_args():
-    result = minimize(func, np.ones(3), args=(5,))
-    assert_allclose(result.x, (0, 1, 2), atol=1e-8)
-    assert_allclose(result.fun, 5)
-    assert result.nfev > 0
-    assert result.njev == 0
+    assert(result.nfev > 0)
+    assert(result.njev == 0)
 
 
 def test_gradient():
     result = minimize(func, np.ones(3), jac=grad)
     assert_allclose(result.x, (0, 1, 2), atol=1e-8)
     assert_allclose(result.fun, 1)
-    assert result.nfev > 0
-    assert result.njev > 0
-
-
-def test_callback():
-    class Callback:
-        trace = []
-        def __call__(self, x):
-            self.trace.append(x)
-    callback = Callback()
-    result = minimize(func, np.ones(3),
-                      callback=callback)
-    assert_allclose(result.x, (0, 1, 2), atol=1e-8)
-    assert_allclose(result.fun, 1)
-    assert result.nfev == len(callback.trace)
-    assert_allclose(callback.trace[0], np.ones(3), atol=1e-2)
-    assert_allclose(callback.trace[-1], result.x, atol=1e-2)
+    assert(result.nfev > 0)
+    assert(result.njev > 0)
