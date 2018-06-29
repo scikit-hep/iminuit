a8994c1b46357318c0a3d98211d5d130c8ca4ed2
Hans Dembinski <hans.dembinski@gmail.com>
2018-06-29 15:00:48 +0200

    be consistent with capitalizing

--- iminuit/util.py
+++ iminuit/util.py
@@ -1,4 +1,4 @@
-"""IMinuit utility functions and classes.
+"""iminuit utility functions and classes.
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
