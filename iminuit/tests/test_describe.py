eeeb689a5058925f4f752313fd83a1a482a49eae
Christoph Deil <Deil.Christoph@gmail.com>
2018-08-24 14:01:49 +0200

    Update iminuit/tests/test_describe.py

--- iminuit/tests/test_describe.py
+++ iminuit/tests/test_describe.py
@@ -35,9 +35,10 @@ def test_functor():
     assert describe(A(), True) == ['x', 'y']
 
 
-# This case can't work on Python 3.7
-# See https://github.com/iminuit/iminuit/issues/270
-@pytest.mark.skipif(sys.version_info >= (3, 7))
+@pytest.mark.skipif(
+    sys.version_info >= (3, 7),
+    reason='Not on Py 3.7; see Github issue 270'
+)
 def test_builtin_by_parsing_doc():
     assert describe(ldexp, True) == ['x', 'i']
 
