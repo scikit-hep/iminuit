56438a5df505529bb017b44b9cad8389311ce628
Christoph Deil <Deil.Christoph@gmail.com>
2018-08-24 13:47:55 +0200

    Small edit in ci-scripts

--- ci-scripts/travis_install_deps.py
+++ ci-scripts/travis_install_deps.py
@@ -2,7 +2,7 @@ import sys
 import os
 import subprocess
 
-build = os.environ['BUILD'].lower()
+build = os.environ['BUILD']
 
 
 def pip_install(packages):
@@ -10,15 +10,15 @@ def pip_install(packages):
 
 
 def main():
-    if build == 'conda':
+    if build == 'CONDA':
         subprocess.check_call(['sh', 'ci-scripts/travis_install_conda.sh'])
-    elif build == 'all':
+    elif build == 'ALL':
         pip_install('cython numpy pytest matplotlib scipy ipython sphinx sphinx_rtd_theme jupyter')
-    elif build in {'test', 'coverage'}:
+    elif build in {'TEST', 'COVERAGE'}:
         pip_install('cython numpy pytest matplotlib scipy ipython sphinx sphinx_rtd_theme jupyter pytest-cov')
-    elif build == 'sdist':
+    elif build == 'SDIST':
         pip_install('cython numpy pytest matplotlib scipy ipython')
-    elif build == 'minimal':
+    elif build == 'MINIMAL':
         pip_install('cython numpy pytest')
     else:
         raise ValueError('build option not recognized: {!r}'.format(build))
