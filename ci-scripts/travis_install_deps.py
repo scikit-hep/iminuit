0e6ee5d7e20c55e4a2b26b1af3bc65317e868761
Christoph Deil <Deil.Christoph@gmail.com>
2018-08-24 12:47:30 +0200

    Fix ci-scripts/travis_install_deps.py

--- ci-scripts/travis_install_deps.py
+++ ci-scripts/travis_install_deps.py
@@ -2,21 +2,27 @@ import sys
 import os
 import subprocess
 
+build = os.environ['BUILD'].lower()
+
 
 def pip_install(packages):
     subprocess.check_call([sys.executable, '-m', 'pip', 'install'] + packages.split())
 
 
-build = os.environ['BUILD'].lower()
-if build == 'conda':
-    subprocess.check_call(['sh', 'install_conda.sh'])
-elif build == 'all':
-    pip_install('cython numpy pytest matplotlib scipy ipython sphinx sphinx_rtd_theme jupyter')
-elif build == 'coverage':
-    pip_install('cython numpy pytest matplotlib scipy ipython sphinx sphinx_rtd_theme jupyter pytest-cov')
-elif build == 'sdist':
-    pip_install('cython numpy pytest matplotlib scipy ipython')
-elif build == 'minimal':
-    pip_install('cython numpy pytest')
-else:
-    raise ValueError('build option not recognized')
+def main():
+    if build == 'conda':
+        subprocess.check_call(['sh', 'ci-scripts/travis_install_conda.sh'])
+    elif build == 'all':
+        pip_install('cython numpy pytest matplotlib scipy ipython sphinx sphinx_rtd_theme jupyter')
+    elif build in {'test', 'coverage'}:
+        pip_install('cython numpy pytest matplotlib scipy ipython sphinx sphinx_rtd_theme jupyter pytest-cov')
+    elif build == 'sdist':
+        pip_install('cython numpy pytest matplotlib scipy ipython')
+    elif build == 'minimal':
+        pip_install('cython numpy pytest')
+    else:
+        raise ValueError('build option not recognized: {!r}'.format(build))
+
+
+if __name__ == '__main__':
+    main()
