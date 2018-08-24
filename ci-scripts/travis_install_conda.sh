9e8a611031cbfd029ffcf7aad951fceb1bc2c2df
Christoph Deil <Deil.Christoph@gmail.com>
2018-08-24 14:12:57 +0200

    Update ci-scripts/travis_install_conda.sh

--- ci-scripts/travis_install_conda.sh
+++ ci-scripts/travis_install_conda.sh
@@ -12,4 +12,4 @@ conda info -a
 # bdist_conda must be installed into a root conda environment,
 # as it imports conda and conda_build. It is included as part of the conda build package.
 conda build --version
-conda install -n root conda-build Cython numpy
+conda install -n root conda-build Cython numpy pytest matplotlib scipy ipython sphinx jupyter
