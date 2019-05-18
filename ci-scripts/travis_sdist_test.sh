a0e93140225cce19868f709dce65f32c8d41b018
Hans Dembinski <hans.dembinski@gmail.com>
2019-05-19 00:45:57 +0200

    fix the tests and remove superfluous stuff

--- ci-scripts/travis_sdist_test.sh
+++ ci-scripts/travis_sdist_test.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 make clean
 make sdist
 virtualenv dist
-source dist/bin/activate
+source dist/bin/activate || { echo "Error: You must execute or source this script!"; exit 1; }
 cd dist
 IMINUIT=$( ls iminuit-*.tar* )
 pip install ${IMINUIT}[tests]
-python -c 'import iminuit; iminuit.test()'
+python -c 'import sys, iminuit; sys.exit(iminuit.test())'
