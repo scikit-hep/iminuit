7dece81e22cfaaa4fdcfafc7f7eb5eb6a3092dbd
Hans Dembinski <hans.dembinski@gmail.com>
2019-05-21 13:01:38 +0200

    use gammapy master

--- .ci/gammapy_integration_test.sh
+++ .ci/gammapy_integration_test.sh
@@ -6,7 +6,6 @@ fi
 source gammapy/bin/activate || { echo "Error: You must execute or source this script!"; exit 1; }
 pip install cython numpy pytest
 pip install .
-pip install gammapy pytest-astropy
-cd /tmp
+pip install pytest-astropy git+https://github.com/gammapy/gammapy@master 
 gammapy info
 python -c "import sys, gammapy; sys.exit(gammapy.test())"
