88917e4ab54e2e144182ee448d64b68692d86ab6
Hans Dembinski <hans.dembinski@gmail.com>
2019-05-24 13:16:10 +0200

    changing name of requirements file

--- .ci/build-wheels.sh
+++ .ci/build-wheels.sh
@@ -9,7 +9,7 @@ pys=(${pys[@]//*34*/})
 
 # Compile wheels
 for PYBIN in "${pys[@]}"; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" install -r /io/requirements-dev.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
