65d2e8f91a6afa07cc767105918256699875c7bc
Hans Dembinski <hans.dembinski@gmail.com>
2019-10-11 12:39:53 +0200

    wip

--- iminuit/tests/test_plot.py
+++ iminuit/tests/test_plot.py
@@ -15,42 +15,54 @@ def f1(x, y):
     return (1 - x) ** 2 + 100 * (y - 1) ** 2
 
 
+def f2(par):
+    return f1(par[0], par[1])
+
+
 @pytest.fixture
-def m():
+def m1():
     m = Minuit(f1, x=0, y=0, pedantic=False, print_level=1)
-    m.tol = 1e-4
     m.migrad()
-    assert_allclose(m.fval, 0, atol=1e-6)
-    assert_allclose(m.values["x"], 1, atol=1e-3)
-    assert_allclose(m.values["y"], 1, atol=1e-3)
+    return m
+
+
+@pytest.fixture
+def m2():
+    m = Minuit(f2, (0, 0), pedantic=False, print_level=1)
+    m.migrad()
     return m
 
 
 @requires_dependency("matplotlib")
+@pytest.mark.parametrize("m", (m1, m2))
 def test_profile(m):
     m.minos("x")
     m.draw_profile("x")
 
 
 @requires_dependency("matplotlib")
+@pytest.mark.parametrize("m", (m1, m2))
 def test_mnprofile(m):
     m.minos("x")
     m.draw_mnprofile("x")
 
 
 @requires_dependency("matplotlib")
+@pytest.mark.parametrize("m", (m1, m2))
 def test_mncontour(m):
     m.minos()
     m.draw_mncontour("x", "y")
 
 
 @requires_dependency("matplotlib")
+@pytest.mark.parametrize("m", (m1, m2))
 def test_drawcontour(m):
     m.minos()
     m.draw_contour("x", "y")
 
 
 @requires_dependency("matplotlib")
+@pytest.mark.parametrize("m", (m1, m2))
 def test_drawcontour_show_sigma(m):
     m.minos()
     m.draw_contour("x", "y", show_sigma=True)
