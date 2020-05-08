1395a61ffd4acdb7255900cc7f406428d53f1608
Hans Dembinski <hans.dembinski@gmail.com>
2020-05-08 21:18:02 +0200

    fix

--- .ci/install_pypy3.6.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3 .ci/install_pypy3.py
-source pypy3.6/pypy3/bin/activate || { echo "Error: You must execute or source this script!"; exit 1; }
-pip install --upgrade cython numpy==1.15.4 pytest
