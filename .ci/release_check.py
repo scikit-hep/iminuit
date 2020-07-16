880ef158f6b91dbb55a0ade448b7908f15893889
Hans Dembinski <hans.dembinski@gmail.com>
2020-07-16 23:35:30 +0200

    fix

--- .ci/release_check.py
+++ .ci/release_check.py
@@ -1,27 +1,23 @@
 import requests
 from pkg_resources import parse_version
-import pathlib
+import sys
 
-dir = pathlib.Path().absolute()
-while dir:
-    dir = dir.parent
-    if dir.glob("setup.py"):
-        break
+version_fn, changelog_fn = sys.argv[1:]
 
-with open(dir / "src" / "iminuit" / "version.py") as f:
+with open(version_fn) as f:
     version = {}
     exec(f.read(), version)  # this loads __version__
     version = version["__version__"]
 
+# make sure that changelog was updated
+with open(changelog_fn) as f:
+    assert version in f.read()
+
+# make sure that version itself was updated
 r = requests.get("https://pypi.org/pypi/iminuit/json")
 releases = r.json()["releases"]
 
 pypi_versions = [parse_version(v) for v in releases]
 this_version = parse_version(version)
 
-# make sure that version was updated
 assert this_version not in pypi_versions
-
-# make sure that changelog was updated
-with open(dir / "doc" / "changelog.rst") as f:
-    assert version in f.read()
