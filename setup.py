f9a8f256583ac5ac222fa0666844c08c96c3c563
Hans Dembinski <HDembinski@users.noreply.github.com>
2020-11-08 18:01:30 +0100

    use cibuildwheels to make wheels (#512)

--- setup.py
+++ setup.py
@@ -37,9 +37,9 @@ setup(
     license="MIT",
     packages=["iminuit"],
     package_dir={"": "src"},
+    install_requires=["numpy"],
     version=version,
     python_requires=">=3.6",
-    zip_safe=False,
     ext_modules=[CMakeExtension("cmake_example")],
     cmdclass=dict(build_ext=CMakeBuild),
     classifiers=[
