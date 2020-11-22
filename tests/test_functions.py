5b5ecde65526913401d0de3f3814b5e6e3f33d3c
Hans Dembinski <hans.dembinski@gmail.com>
2020-11-22 12:22:36 +0100

    importable test functions

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,31 +1,23 @@
-from math import sqrt, exp, cos, pi, e
 from iminuit import Minuit
+from iminuit.testing import rosenbrock, rosenbrock_grad, ackley, beale, matyas
 from numpy.testing import assert_allclose
+import pytest
 
 
-def rosenbrock(x, y):
-    """Rosenbrock function, minimum at (1, 1) with value of 0."""
-    return (1 - x) ** 2 + 100 * (y - x ** 2) ** 2
-
-
-def test_rosenbrock():
-    m = Minuit(rosenbrock, x=0, y=0)
+@pytest.mark.parametrize("grad", (None, rosenbrock_grad))
+def test_rosenbrock(grad):
+    m = Minuit(rosenbrock, x=0, y=0, grad=grad)
     m.tol = 1e-4
     m.errordef = 1
     m.migrad()
     assert_allclose(m.fval, 0, atol=1e-6)
     assert_allclose(m.values["x"], 1.0, atol=1e-3)
     assert_allclose(m.values["y"], 1.0, atol=1e-3)
+    print(m)
 
 
-def ackleys(x, y):
-    term1 = -20 * exp(-0.2 * sqrt(0.5 * (x ** 2 + y ** 2)))
-    term2 = -exp(0.5 * (cos(2 * pi * x) + cos(2 * pi * y)))
-    return term1 + term2 + 20 + e
-
-
-def test_ackleys():
-    m = Minuit(ackleys, x=0.3, y=-0.2)
+def test_ackley():
+    m = Minuit(ackley, x=0.3, y=-0.2)
     # m.errors = 1.7
     m.tol = 1e-4
     m.errordef = 1
@@ -33,13 +25,6 @@ def test_ackleys():
 
     assert_allclose(m.fval, 0, atol=1e-6)
     assert_allclose(m.values, [0, 0], atol=1e-6)
-
-
-def beale(x, y):
-    term1 = 1.5 - x + x * y
-    term2 = 2.25 - x + x * y ** 2
-    term3 = 2.625 - x + x * y ** 3
-    return term1 * term1 + term2 * term2 + term3 * term3
 
 
 def test_beale():
@@ -50,10 +35,6 @@ def test_beale():
     assert_allclose(m.fval, 0, atol=1e-6)
     assert_allclose(m.values, [3, 0.5], atol=1e-3)
     assert m.fval < 1e-6
-
-
-def matyas(x, y):
-    return 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
 
 
 def test_matyas():
