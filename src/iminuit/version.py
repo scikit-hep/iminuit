86ac4351ec5413193b35167dff984e66e923868b
Hans Dembinski <hans.dembinski@gmail.com>
2022-07-16 11:22:55 +0200

    update version to beta

--- src/iminuit/version.py
+++ src/iminuit/version.py
@@ -7,7 +7,7 @@
 # - Increase MAINTENANCE when fixing bugs without adding features
 # - During development, add suffix .devN with N >= 0
 # - For release candidates, add suffix .rcN with N >= 0
-version = "2.12.2"
+version = "2.12.3.beta1"
 
 # We list the corresponding ROOT version of the C++ Minuit2 library here
 root_version = "v6-25-02-1013-ga4bb8f3342"
