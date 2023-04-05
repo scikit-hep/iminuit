60d2a5c80e0225b550cbd2ca0e5b63be28120eef
Hans Dembinski <hans.dembinski@gmail.com>
2023-04-05 12:07:44 +0200

    fix

--- src/iminuit/_optional_dependencies.py
+++ src/iminuit/_optional_dependencies.py
@@ -4,12 +4,15 @@ from iminuit.warnings import OptionalDependencyWarning
 
 
 @contextlib.contextmanager
-def optional_module_for(functionality, stacklevel=3):
+def optional_module_for(functionality, *, replace=None, stacklevel=3):
     try:
         yield
     except ModuleNotFoundError as e:
+        package = e.name.split(".")[0]
+        if replace:
+            package = replace.get(package, package)
         msg = (
-            f"{functionality} requires optional package {e.name!r}. "
-            f"Install {e.name!r} manually to enable this."
+            f"{functionality} requires optional package {package!r}. "
+            f"Install {package!r} manually to enable this functionality."
         )
         warnings.warn(msg, OptionalDependencyWarning, stacklevel=stacklevel)
--- src/iminuit/warnings.py
+++ src/iminuit/warnings.py
@@ -1,12 +1,12 @@
 """Warnings used by iminuit."""
 
 
-class OptionalDependencyWarning(RuntimeWarning):
-    """Feature requires an optional external package."""
-
-
 class IMinuitWarning(RuntimeWarning):
     """Generic iminuit warning."""
+
+
+class OptionalDependencyWarning(IMinuitWarning):
+    """Feature requires an optional external package."""
 
 
 class HesseFailedWarning(IMinuitWarning):
