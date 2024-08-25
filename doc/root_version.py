c5254770c7b7ac3459d9071756000a2ccd3cf699
Hans Dembinski <hans.dembinski@gmail.com>
2024-08-25 18:32:50 +0200

    fix

--- doc/root_version.py
+++ doc/root_version.py
@@ -2,26 +2,15 @@ import subprocess as subp
 from pathlib import PurePath
 
 project_dir = PurePath(__file__).parent.parent
-
-# check that root version is up-to-date
-git_submodule = subp.check_output(
-    ["git", "submodule", "status"], cwd=project_dir
-).decode()
-
-for item in git_submodule.strip().split("\n"):
-    parts = item.split()
-    if PurePath(parts[1]) != PurePath("extern") / "root":
-        continue
-
-    assert len(parts) == 3, "module is not checked out"
-    break
-
 root_dir = project_dir / "extern" / "root"
+
+subp.check_call(["git", "checkout", "master"], cwd=root_dir)
+git_submodule = subp.check_output(
+    ["git", "submodule", "update"], cwd=project_dir
+).decode()
 
 # git submodule status does not yield the right state
 # we must use git describe --tags
-subp.check_call(["git", "pull", "--tags"], cwd=root_dir)
-
 root_version = (
     subp.check_output(["git", "describe", "--tags"], cwd=root_dir).decode().strip()
 )
