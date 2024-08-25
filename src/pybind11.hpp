6a5ae872289759d2eea74c8c04f1e1046bad867c
Hans Dembinski <hans.dembinski@gmail.com>
2024-08-25 16:51:36 +0200

    fix

--- src/pybind11.hpp
+++ src/pybind11.hpp
@@ -1,6 +1,10 @@
 #ifndef PYBIND11_HPP
 #define PYBIND11_HPP
 
+// We must load pybind11 consistently through this header
+// to ensure that our type_caster specialization is used
+// in every translation unit. Otherwise we get ODR violations.
+
 #include <pybind11/numpy.h>
 #include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
@@ -11,6 +15,9 @@
 namespace pybind11 {
 namespace detail {
 
+// This specialization for vector<double> acts as an override from the more
+// generic template in pybind11/stl.h. We use it to cast the C++ vector
+// into a numpy array instead of a Python list.
 template <>
 struct type_caster<std::vector<double>> {
   using vec_t = std::vector<double>;
@@ -33,7 +40,6 @@ struct type_caster<std::vector<double>> {
     return false;
   }
 
-public:
   template <typename T>
   static handle cast(T&& src, return_value_policy, handle) {
     array_t<double> arr(static_cast<ssize_t>(src.size()));
