1ba806509c8ee09633a78bfa31fb4133c5a8dfb1
Adrian Peter Krone <adrian.krone97@gmail.com>
2024-12-11 20:39:16 +0100

    Removed unnecessary lines

--- tests/test_without_pyqt6.py
+++ tests/test_without_pyqt6.py
@@ -10,14 +10,7 @@ def test_interactive(qtbot):
     import iminuit
 
     cost = LeastSquares([1.1, 2.2], [3.3, 4.4], 1, lambda x, a: a * x)
-    mw = iminuit.Minuit(cost, 1).interactive(qt_exec=False)
-    qtbot.addWidget(mw)
-    mw.close()
-    mw.deleteLater()
 
     with hide_modules("PyQt6", reload="iminuit.qtwidget"):
         with pytest.raises(ModuleNotFoundError, match="Please install"):
-            mw = iminuit.Minuit(cost, 1).interactive(qt_exec=False)
-            qtbot.addWidget(mw)
-            mw.close()
-            mw.deleteLater()
+            iminuit.Minuit(cost, 1).interactive()
