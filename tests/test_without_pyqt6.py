a8ad0dc0de40d13088cc62e58cdfdab408e31313
Adrian Peter Krone <adrian.krone@ptoton.me>
2024-12-19 15:53:37 +0100

    Test both AttributeError and ImportError in is_jupyter

--- tests/test_without_pyqt6.py
+++ tests/test_without_pyqt6.py
@@ -2,13 +2,25 @@ from iminuit._hide_modules import hide_modules
 from iminuit.cost import LeastSquares
 import pytest
 
+pytest.importorskip("matplotlib")
 
-def test_interactive():
-    pytest.importorskip("matplotlib")
+
+def test_pyqt6_interactive_with_ipython():
+    pytest.importorskip("IPython")
     import iminuit
 
     cost = LeastSquares([1.1, 2.2], [3.3, 4.4], 1, lambda x, a: a * x)
 
     with hide_modules("PyQt6", reload="iminuit.qtwidget"):
+        with pytest.raises(ModuleNotFoundError, match="Please install PyQt6"):
+            iminuit.Minuit(cost, 1).interactive()
+
+
+def test_pyqt6_interactive_without_ipython():
+    import iminuit
+
+    cost = LeastSquares([1.1, 2.2], [3.3, 4.4], 1, lambda x, a: a * x)
+
+    with hide_modules("PyQt6", "IPython", reload="iminuit.qtwidget"):
         with pytest.raises(ModuleNotFoundError, match="Please install PyQt6"):
             iminuit.Minuit(cost, 1).interactive()
